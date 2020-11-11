32353550d9ba0dbfe34b9236d2a5ebc1878ee3d2
dbouris <bourisd2@gmail.com>
2020-11-11 23:42:20 +0200

    CREATING THE FIRST FILES OF THE PROGRAMM

---
+++ Hello.java
@@ -0,0 +1,13 @@
+
+
+public class Hello {
+	    public static void main(String[] args) {
+		    pao.ret();      
+		    System.out.println("JUST TRYING TO CODE FROM THE COMAND LINE");
+			   
+	    }
+
+}
+
+
+
---
+++ pao.java
@@ -0,0 +1,7 @@
+public class pao {
+public static  void ret(){
+System.out.println("MY TEAM IS PAO");
+
+}
+
+}
